99771f56a519225c53e33a7fbb0f7a66e81d9a6d
droodev <droodev@gmail.com>
2024-09-09 11:17:03 -0500

    Updated swap_distance_test to compare the C++ imp against the Python imp, and not against the ILP one.

--- tests/func/mapof/elections/distances/swap_distance_test.py
+++ tests/func/mapof/elections/distances/swap_distance_test.py
@@ -1,20 +1,22 @@
 import mapof.elections as mapel
+from mapof.elections.distances import main_ordinal_distances as mod
 
 
-# class TestSwapDistance:
-#    def test_bf_vs_ilp_swap_distance(self):
-#        for _ in range(20):
-#            election_1 = mapel.generate_ordinal_election(culture_id='ic',
-#                                                         num_voters=5,
-#                                                         num_candidates=3)
-#            election_2 = mapel.generate_ordinal_election(culture_id='ic',
-#                                                         num_voters=5,
-#                                                         num_candidates=3)
-#
-#            distance_1, _ = mapel.compute_distance(election_1, election_2,
-#                                                   distance_id='swap')
-#
-#            distance_2, _ = mapel.compute_distance(election_1, election_2,
-#                                                   distance_id='ilp_swap')
-#
-#            assert distance_1 == distance_2, "BF swap distance differs from ILP swap distance"
+class TestSwapDistance:
+   def test_bf_vs_ilp_swap_distance(self):
+       for _ in range(5):
+           election_1 = mapel.generate_ordinal_election(culture_id='ic',
+                                                        num_voters=5,
+                                                        num_candidates=3)
+           election_2 = mapel.generate_ordinal_election(culture_id='ic',
+                                                        num_voters=5,
+                                                        num_candidates=3)
+
+           distance_1, _ = mapel.compute_distance(election_1, election_2,
+                                                  distance_id='swap')
+
+           distance_2, _ = mod.compute_swap_distance_bf(election_1,
+                                                        election_2)
+
+           assert distance_1 == distance_2, "C++ BF swap distance differs from \
+           Python BF swap distance"

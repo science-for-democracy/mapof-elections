76bc3e63444a1fc96fa85cf827325847b6160b4a
szufa <s.szufa@gmail.com>
2024-09-25 12:37:25 +0200

    add new class tests

---
+++ tests/func/mapof/elections/objects/test_ApprovalExperiment.py
@@ -0,0 +1,13 @@
+
+import mapof.elections as mapof
+
+
+class TestApprovalExperiment:
+
+    def test_online_approval_experiment(self):
+
+        experiment = mapof.prepare_online_approval_experiment()
+
+
+
+
---
+++ tests/func/mapof/elections/objects/test_OrdinalExperiment.py
@@ -0,0 +1,13 @@
+
+import mapof.elections as mapof
+
+
+class TestOrdinalExperiment:
+
+    def test_online_ordinal_experiment(self):
+
+        experiment = mapof.prepare_online_ordinal_experiment()
+
+
+
+
